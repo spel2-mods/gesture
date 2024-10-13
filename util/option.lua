5fb2c5395ad50cd70c4231dbfdfb74f07364e2cd
fienestar <fienestar@gmail.com>
2024-10-14 01:17:06 +0900

    add resigter_hidden_option family

--- util/option.lua
+++ util/option.lua
@@ -22,6 +22,33 @@ module.register_option_bool = wrap_register_option(register_option_bool, 4)
 module.register_option_string = wrap_register_option(register_option_string, 4)
 module.register_option_combo = wrap_register_option(register_option_combo, 5)
 
+---@generic T
+---@param _ T @extends function
+---@param default_value_at integer
+---@return T
+local function wrap_register_hidden_option(_, default_value_at)
+    return function(...)
+        local args = {...}
+        local name = args[1]
+        local default_value = args[default_value_at]
+        module.default_values[name] = default_value
+    end
+end
+
+--- options only can be edited via save file
+--- same interface as register_option_*
+module.register_hidden_option_int = wrap_register_hidden_option(register_option_int, 4)
+module.register_hidden_option_float = wrap_register_hidden_option(register_option_float, 4)
+module.register_hidden_option_bool = wrap_register_hidden_option(register_option_bool, 4)
+module.register_hidden_option_string = wrap_register_hidden_option(register_option_string, 4)
+module.register_hidden_option_combo = wrap_register_hidden_option(register_option_combo, 5)
+
+---@param name string
+---@param value any
+module.register_hidden_option = function(name, value)
+    module.default_values[name] = value
+end
+
 ---@param name string
 function module.unregister_option(name)
     module.default_values[name] = nil
